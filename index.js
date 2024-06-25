ad573d84ef7ea3840757e21411cc226feb3f596c
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-25 17:41:51 +0300

    creates an anonymous function

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function receivesAFunction (callback){
 
 function returnsANamedFunction (){
   return function namedFunction(){
-    
+
   }
+}
+
+function returnsAnAnonymousFunction (){
+  return function (){
+    
+      }
 }
